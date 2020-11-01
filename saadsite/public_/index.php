907a17bb2052e60c65d2c2b781b48b2347f395f6
saadm2002 <72601361+saadm2002@users.noreply.github.com>
2020-11-01 16:55:20 +0000

    Create index.php

---
+++ saadsite/public_/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Homepage</title>
+<link rel="stylesheet" type="text/css" href="Background.css">
+<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+
+<?php include 'navbar.php';?>
+
+<div class="background">
+
+<h1>Welcome To A-Town Trimz</h1>
+<p>This is our official website. You can book your appointments by using our
+  online booking service. We will also post pictures of our recent haircuts and
+  display our latest customer reviews. Please drop a review of our services we
+  have provided you so we can improve.</p>
+
+</div>
+</body>
+</html>
