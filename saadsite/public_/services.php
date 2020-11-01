11f89cc8e21e6948ae4f7f4669433cd0746ef3d4
saadm2002 <72601361+saadm2002@users.noreply.github.com>
2020-11-01 16:54:56 +0000

    Create services.php

---
+++ saadsite/public_/services.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Services</title>
+<link rel="stylesheet" type="text/css" href="Background.css">
+<link rel="stylesheet" type="text/css" href="services.css">
+</head>
+<body>
+
+<?php include 'navbar.php';?>
+
+<div class="background">
+
+<div class="row">
+
+<div class="column">
+<div class="container">
+<img src="All.jpg" style="width:200%">
+<div class="overlay">
+<div class="text">All (Haircut/Skin Fade + Beard + Shape Up) - £15</div>
+</div>
+</div>
+<div class="container">
+<img src="Beard.jpg" style="width:200%">
+<div class="overlay">
+<div class="text">Beard Shape Up/ Line Up - £4</div>
+</div>
+</div>
+</div>
+
+<div class="column">
+<div class="container">
+<img src="SkinFade.jpg" style="width:200%">
+<div class="overlay">
+<div class="text">Skin Fade (Low/Mid/High/Temple/Burst) - £8</div>
+</div>
+</div>
+<div class="container">
+<img src="HotTowelShave.jpg" style="width:200%">
+<div class="overlay">
+<div class="text">Hot Towel Shave - £5</div>
+</div>
+</div>
+</div>
+
+<div class="column">
+<div class="container">
+<img src="Haircut.jpg" style="width:200%">
+<div class="overlay">
+<div class="text">Haircut (Machine Cut/ Scissor Cut) - £7</div>
+</div>
+</div>
+<div class="container">
+<img src="ShapeUp.jpg" style="width:200%">
+<div class="overlay">
+<div class="text">Shape Up - £3</div>
+</div>
+</div>
+</div>
+
+</div>
+</body>
+</html>
